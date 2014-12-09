239f711b38ccc7fc7d1e90e8672cafbd82bd93d1
esanzy <esanzy87@gmail.com>
2014-12-09 21:42:06 +0900

    메인디자인 수정

--- cmcprj/cmcprj/settings/local.py
+++ cmcprj/cmcprj/settings/local.py
@@ -16,7 +16,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.mysql',
         'NAME': 'cmcweb',
         'USER': 'root',
-        'PASSWORD': 'nimd@123!',
+        'PASSWORD': 'btj1040!',
         'HOST': '127.0.0.1',
         'PORT': '3306',
     }
